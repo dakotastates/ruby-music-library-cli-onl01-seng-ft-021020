178841d97fc1b8d7b58054ea5529330dfe414680
Dakota States <dakota.states@gmail.com>
2020-03-02 07:13:15 +0000

    Automatically backed up by Learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -1,25 +1,25 @@
-class Artist 
-  
-  attr_accessor :name, :song
-  
+class Artist
+  attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
+  extend Concerns::Findable
   @@all = []
+
   def initialize(name)
     @name = name
     @songs = []
   end
-  
+
   def self.all
     @@all
   end
-  
+
+  def self.destroy_all
+    @@all.clear
+  end
+
   def save
     @@all << self
   end
-  
-  def self.destory_all
-    @@all.clear
-  end
-  
+
   def self.create(artist)
     artist = self.new(artist)
     artist.save
@@ -29,4 +29,31 @@ class Artist
   def songs
     @songs
   end
+
+  def add_song(song)
+    if song.artist == nil
+      song.artist = self
+    else
+      nil
+    end
+    if @songs.include?(song)
+      nil
+    else
+      @songs << song
+    end
+    song
+  end
+
+  def genres
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.genre)
+        nil
+      else
+        @new_array << song.genre
+      end
+    end
+    @new_array
+  end
+
 end
